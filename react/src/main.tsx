7760c9820ab3c5ab8e6a1583100fadb3cbb9ec3c
trif0lium <gh@fleeingsunlight.dev>
2021-11-16 19:48:19 +0700

    feat: add GlobalStyle

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { Normalize } from 'styled-normalize'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://rsms.me/inter/inter.css');
+  html { font-family: 'Inter', sans-serif; }
+  @supports (font-variation-settings: normal) {
+    html { font-family: 'Inter var', sans-serif; }
+  }
+`
 
 ReactDOM.render(
   <React.StrictMode>
     <Normalize />
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
