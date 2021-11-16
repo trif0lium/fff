e689e51bd9c203c87ecd86de4334f1e4e3de677c
new5558 <new17353@gmail.com>
2021-11-16 20:23:02 +0700

    feat: add hero content

--- react/src/components/Hero.tsx
+++ react/src/components/Hero.tsx
@@ -1,3 +1,60 @@
-import React from 'react'
+import styled from 'styled-components';
 
-export const Hero = () => <></>
+const HeroTitle = styled.span`
+  font-size: 10rem;
+  line-height: 9rem;
+`;
+
+const HeroTitleContainer = styled.h1`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin-bottom: 64px;
+`;
+
+const HeroContainer = styled.div``;
+const ActionContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const ActionButton = styled.button`
+  padding: 0 25px;
+  border: 1px solid black;
+  color: #ffffff;
+  background-color: black;
+  border-radius: 0.25rem;
+  height: 50px;
+  font-weight: 500;
+  line-height: 38px;
+  font-size: 1rem;
+  cursor: pointer;
+  margin-bottom: 64px;
+`;
+
+const HeroDescription = styled.div`
+  line-height: 1.6em;
+  color: #666;
+  font-weight: 400;
+  font-size: 1.25rem;
+  text-align: center;
+`;
+
+export const Hero = () => {
+  return (
+    <HeroContainer>
+      <HeroTitleContainer>
+        <HeroTitle>Friday.</HeroTitle>
+        <HeroTitle>For.</HeroTitle>
+        <HeroTitle>Future.</HeroTitle>
+      </HeroTitleContainer>
+      <ActionContainer>
+        <ActionButton>Start Action Today</ActionButton>
+      </ActionContainer>
+      <HeroDescription>
+        Vercel combines the best developer experience with an obsessive focus on end-user performance. Our platform
+        enables frontend teams to do their best work.
+      </HeroDescription>
+    </HeroContainer>
+  );
+};
