53f22ca61ebf50fdc516d7b1b8ebaf6cc09c5522
trif0lium <gh@fleeingsunlight.dev>
2021-11-16 20:14:20 +0700

    feat: add Footer component

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,3 +1,4 @@
+import { Footer } from './components/Footer'
 import { Hero } from './components/Hero'
 import { HowToStrike } from './components/HowToStrike'
 import { ReasonsToStrike } from './components/ReasonsToStrike'
@@ -10,6 +11,7 @@ function App() {
       <ReasonsToStrike />
       <HowToStrike />
       <SocialMedia />
+      <Footer />
     </div>
   )
 }
