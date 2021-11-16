76bff4717bc67c2ddab12514a29aeb4a5fef365a
Norapat Buppodom <new17353@gmail.com>
2021-11-16 21:21:47 +0700

    feat: add social mdeia section (#4)

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,22 +1,22 @@
-import { Container } from './components/Container'
-import { Footer } from './components/Footer'
-import { Hero } from './components/Hero'
-import { HowToStrike } from './components/HowToStrike'
-import { ReasonsToStrike } from './components/ReasonsToStrike'
-import { SocialMedia } from './components/SocialMedia'
+import { Container } from './components/Container';
+import { Footer } from './components/Footer';
+import { Hero } from './components/Hero';
+import { HowToStrike } from './components/HowToStrike';
+import { ReasonsToStrike } from './components/ReasonsToStrike';
+import { SocialMedia } from './components/SocialMedia';
 
 function App() {
   return (
     <div className="App">
-    <Container>
-      <Hero />
-      <ReasonsToStrike />
-      <HowToStrike />
-      <SocialMedia />
-      <Footer />
-    </Container>
+      <Container>
+        <Hero />
+        <ReasonsToStrike />
+        <HowToStrike />
+        <SocialMedia />
+        <Footer />
+      </Container>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
