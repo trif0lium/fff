612ffd0e0e227261f82285801cc3fb40e7b997db
trif0lium <gh@fleeingsunlight.dev>
2021-11-16 20:50:01 +0700

    feat(App): add Container component

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,3 +1,4 @@
+import { Container } from './components/Container'
 import { Footer } from './components/Footer'
 import { Hero } from './components/Hero'
 import { HowToStrike } from './components/HowToStrike'
@@ -7,11 +8,13 @@ import { SocialMedia } from './components/SocialMedia'
 function App() {
   return (
     <div className="App">
+    <Container>
       <Hero />
       <ReasonsToStrike />
       <HowToStrike />
       <SocialMedia />
       <Footer />
+    </Container>
     </div>
   )
 }
