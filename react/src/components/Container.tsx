b3fa16477f03f97946e705b59482fa244621c172
trif0lium <gh@fleeingsunlight.dev>
2021-11-16 20:54:10 +0700

    feat(components/Container): add `margin` 24px

--- react/src/components/Container.tsx
+++ react/src/components/Container.tsx
@@ -5,5 +5,5 @@ export interface Props {
 }
 
 export const Container: React.FC<Props> = ({ children }: Props) => {
-  return <div style={{ maxWidth: "1200px", width: "100%", margin: "0 auto" }}>{children}</div>
+  return <div style={{ maxWidth: "1200px", width: "100%", margin: "0 auto" }}><div style={{ margin: "0 24px" }}>{children}</div></div>
 }
