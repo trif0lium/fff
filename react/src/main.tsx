937399ad04db6d32f5580540c21d29a72618d24c
trif0lium <gh@fleeingsunlight.dev>
2021-11-16 18:59:19 +0700

    feat: add Normalize

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { Normalize } from 'styled-normalize'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Normalize />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
