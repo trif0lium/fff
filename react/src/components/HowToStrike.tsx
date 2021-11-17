06b8b85c986ac791f8688b9acef0186ea77842ca
trif0lium <gh@fleeingsunlight.dev>
2021-11-17 13:57:27 +0700

    paraphrase -> main (#8)
    * chore(components/SocialMedia): 40px -> 32px
    
    * chore(components/ReasonsToStrike): paraphrase
    
    * chore(components/HowToStrike): paraphrase
    
    * chore(components/HowToStrike): paraphrase "Amplify the message" section
    
    * chore(components/HowToStrike): paraphrase "Get Permissions?" section
    
    * chore(components/HowToStrike): paraphrase "Choosing Where and When to strike" section
    
    * chore(components/HowToStrike): paraphrase "Alternatives to physical strikes"
    
    * chore(components/HowToStrike): paraphrase "Stay Safe" section
    
    * chore(components/HowToStrike): remove "Getting Start" section
    
    * chore(components/HowToStrike): format "Amplify the Message" section

--- react/src/components/HowToStrike.tsx
+++ react/src/components/HowToStrike.tsx
@@ -16,71 +16,23 @@ export const HowToStrike = () => {
     <div style={{ display: "flex", justifyContent: "center" }}>
       <div style={{ maxWidth: '600px' }}>
         <div style={{ marginBottom: '64px' }}>
-          <Paragraph>
-            Talk to people you know (family, friends) and try to get them to understand how serious the situation is.
-            Start small. You can strike on your own, but it is often more fun and more productive to get some friends
-            together. Decide on a time and place to strike and invite members of local climate justice groups and
-            environmental groups. The more people the better!
-          </Paragraph>
+          <Paragraph>Talk to people you know (family, friends) and try to persuade them of the gravity of the situation. Begin with a small project. You can strike on your own, but it's usually more enjoyable and productive to gather a group of friends. Decide on a strike date and location, and invite members of local climate justice and environmental organizations to join you. The more people there are, the better!</Paragraph>
         </div>
           <div style={{ marginBottom: '64px' }}>
-            <Title>Getting Start</Title>
-            <Paragraph>Register your strike on this webpage here.</Paragraph>
-          </div>
-          <div style={{ marginBottom: '64px' }}>
-            <Title>Amplify the message</Title>
-            <Paragraph>
-              Check out the “contact” pages on the websites of newspapers and TV stations in your area. Send them an
-              announcement of the time and place of your strike, letting them know there will be signs and banners that
-              will make great photos. During the strike, consider taking a few photos and sending them to media outlets.
-              On social media, ask friends with many followers to boost your message. Don’t be shy! This is important
-              work.
-              <br />
-              Don’t forget to share your achievements on social media by hashtagging #FridaysForFuture and
-              #ClimateStrike. We need you! It is together that we will make a difference. 
-            </Paragraph>
+            <Title>Amplify the Message</Title>
+            <Paragraph>Examine the "contact" pages on the websites of local newspapers and television stations. Send them an announcement about the time and location of your strike, as well as the presence of signs and banners that will make for great photos. Consider taking a few photos and sending them to media outlets during the strike. On social media, ask friends who have a large number of followers to share your message. Don't be afraid to ask! This is critical work.<br /><br />Remember to share your accomplishments on social media with the hashtags #FridaysForFuture and #ClimateStrike. We require your assistance! We will make a difference only if we work together.</Paragraph>
           </div>
           <div style={{ marginBottom: '64px' }}>
             <Title>Get Permissions?</Title>
-            <Paragraph>
-              Many of our strikes have been approved by the police. If you’re organizing a strike, you might want to
-              check if you need police approval.  Often these approvals only allow for a quiet, sitting protest.  Check
-              the laws in your area if you want to use any amplified noises like music or loudspeakers for speeches.
-              This may require a permit as well.
-              <br />
-              Fridays For Future is unlikely to be able to provide legal support should problems occur, so if they do,
-              we would advise liaising with NGOs in your area who know local lawyers, well versed in protest
-              situations. 
-            </Paragraph>
+            <Paragraph>The cops have approved many of our strikes. If you're planning a strike, you should see if you require police permission. Frequently, these licenses only permit a silent, sitting protest. If you want to utilize any amplified noises for speeches, such as music or loudspeakers, check your local rules. This may also necessitate obtaining a permit. Fridays For Future is unlikely to be able to help with legal issues, therefore if they arise, we recommend contacting local NGOs who know local lawyers who are familiar with protest circumstances.</Paragraph>
           </div>
           <div style={{ marginBottom: '64px' }}>
             <Title>Choosing Where and When to strike</Title>
-            <Paragraph>
-              You’re welcome to follow Greta’s lead and strike in front of your closest town hall, city hall or other
-              local government building every Friday from 8.00-15.00, including holidays.
-              <br />
-              Many weekly strikes are shorter and start at noon.
-              <br />
-              Striking in front of a publicly owned building and in the same place and time each week makes it easier
-              for others to join.
-            </Paragraph>
+            <Paragraph>Every Friday from 8 a.m. to 15 a.m., including holidays, you are welcome to strike in front of your nearest town hall, city hall, or other local government building, following Greta's lead. Many weekly strikes last only a few hours and begin at midday. Striking each week in front of a publicly owned building and at the same time and location makes it simpler for others to join.</Paragraph>
           </div>
           <div style={{ marginBottom: '64px' }}>
-            <Title>Alternatives to physical strikes</Title>
-            <Paragraph>
-              Many people are unable to strike in person. They might not want to miss school or work, or it is not safe
-              for them to do so, or they may have disabilities that make it difficult or impossible to sit or stand
-              outside for long periods. However, there are many alternative possibilities.
-              <br />
-              Striking indoors with a sign and hashtag on social media (like #FridaysForFuture; #Climatestrikeonline,
-              #Digitalstrike) totally counts – so have fun and get creative! Write and perform a climate strike song!
-              The possibilities are endless.
-              <br />
-              You can also sign petitions in Actions and communicate them on social media.
-              <br />
-              Learning more about the climate crisis is also important, and turning knowledge into lists of things you
-              and your family will do.
-            </Paragraph>
+            <Title>Alternatives to Physical Strikes</Title>
+            <Paragraph>Many people are unable to strike physically. They may not want to miss school or work because it is unsafe for them, or they may have disabilities that make it difficult or impossible for them to sit or stand outside for long periods of time. However, there are numerous other options.<br /><br />Striking indoors with a banner and a hashtag on social media (such as #FridaysForFuture; #Climatestrikeonline, #Digitalstrike) counts as well, so have fun and be creative! Make a song about the climate strike and perform it! The options are limitless.<br /><br />You can also sign live petitions and share them on social media. It's also crucial to learn more about the climate catastrophe and apply that information into action plans for you and your family.</Paragraph>
           </div>
           <div style={{ marginBottom: '64px' }}>
             <Title>Fridays For Future Strike Culture</Title>
@@ -98,11 +50,7 @@ export const HowToStrike = () => {
           </div>
           <div>
             <Title>Stay Safe</Title>
-            <Paragraph>
-              Fridays For Future only want you to strike in a way that you feel safe and comfortable with. We are a
-              peaceful organization, but we have seen cases of counter protestors, so please take care to prioritize
-              your safety. 
-            </Paragraph>
+            <Paragraph>Fridays For Future simply wants you to strike in a manner that makes you feel safe and secure. Although we are a peaceful organization, we have seen instances of counter-protestors, so please prioritize your safety.</Paragraph>
           </div>
         </div>
     </div>
