4a6847c714bee0ca4bf403e308e8039198fcd80e
trif0lium <gh@fleeingsunlight.dev>
2021-11-17 10:00:40 +0700

    feat(components): SectionLabel

--- react/src/components/SectionLabel.tsx
+++ react/src/components/SectionLabel.tsx
@@ -1,3 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export const SectionLabel = () => <></>
+interface Props {
+  number: number
+  heading: string
+  startColor: string
+  endColor: string
+}
+
+interface HeadingProps {
+  startColor: string
+  endColor: string
+}
+
+const Heading = styled.h2<HeadingProps>`
+  font-size: 2rem;
+  font-weight: 700;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-image: linear-gradient(90deg, ${props => props.startColor ?? '#000'}, ${ props => props.endColor ?? '#000'})
+`
+
+export const SectionLabel: React.FC<Props> = ({ number, heading, startColor, endColor }: Props) => {
+  return (<>
+    <div style={{ display: "flex" }}>
+      <span style={{ width: "40px", height: "40px", borderRadius: "1000", background: `linear-gradient(90deg, ${startColor}, ${endColor})` }}>{number}</span>
+      <Heading startColor={startColor} endColor={endColor}>{heading}</Heading>
+    </div>
+  </>)
+}
