34e9e9f32b8cc2fa3561346d6db4ccd8983ad1f6
trif0lium <gh@fleeingsunlight.dev>
2021-11-17 10:03:29 +0700

    feat(App): add SectionLabel components

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -3,6 +3,7 @@ import { Footer } from './components/Footer';
 import { Hero } from './components/Hero';
 import { HowToStrike } from './components/HowToStrike';
 import { ReasonsToStrike } from './components/ReasonsToStrike';
+import { SectionLabel } from './components/SectionLabel';
 import { SocialMedia } from './components/SocialMedia';
 
 function App() {
@@ -10,8 +11,11 @@ function App() {
     <div className="App">
       <Container>
         <Hero />
+        <SectionLabel number={1} heading="Reasons To Strike" startColor="#007CF0" endColor="#00DFD8" />
         <ReasonsToStrike />
+        <SectionLabel number={2} heading="How To Strike" startColor="#7928CA" endColor="#FF0080" />
         <HowToStrike />
+        <SectionLabel number={3} heading="Social Media" startColor="#FF4D4D" endColor="#F9CB28" />
         <SocialMedia />
         <Footer />
       </Container>
